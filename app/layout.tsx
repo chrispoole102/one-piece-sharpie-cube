4dc2caa6be9496832e79ad112984da2f6f726399
Christopher Poole <chrispoole102@gmail.com>
2025-12-15 23:01:44 -0800

    added metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,14 +3,22 @@ import "./globals.css";
 import NavBar from "@/app/components/NavBar";
 import Header from "@/app/components/Header";
 
-export const metadata: Metadata = {
-  title: "One Piece Sharpie Cube",
-  description: "Learn more about the One Piece Sharpie Cube, view the card list and simulate pack openings",
-};
 
 export default function RootLayout({children}: Readonly<{ children: React.ReactNode; }>) {
   return (
     <html lang="en">
+        <head>
+            <title>One Piece Sharpie Cube</title>
+            <meta charSet="UTF-8"/>
+            <meta name="description" content="Learn more about the One Piece Sharpie Cube, view the card list and simulate pack openings!"/>
+            <meta name="keywords" content="One Piece, OPTCG, Trading Card Game, Cube, Cube Draft, Sharpie Cube"/>
+            <meta name="author" content="Christopher Poole"/>
+
+            <meta property="og:image" content="https://one-piece-sharpie-cube-cards.s3.us-east-1.amazonaws.com/share-image.jpg" />
+            <meta property="og:description"
+                content="Learn more about the One Piece Sharpie Cube, view the card list and simulate pack openings!" />
+            <meta property="og:title" content="One Piece Sharpie Cube" />
+        </head>
       <body>
       <div className={'flex flex-col'}>
           <Header></Header>
